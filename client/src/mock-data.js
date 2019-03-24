2e7b7f5bed065b6558ce67e78d4523a34ade6a77
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-03-24 12:05:48 -0700

    correct few things of mock data

--- client/src/mock-data.js
+++ client/src/mock-data.js
@@ -1,88 +1,88 @@
-var data = [{
-  id: 1,
-  name: "affenpinscher",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"
-},
-{
-  id: 2,
-  name: "african",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg"
-},
-{
-  id: 3,
-  name: "airedale",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg"
-},
-{
-  id: 4,
-  name: "akita",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"
-},
-{
-  id: 5,
-  name: "appenzeller",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg"
-},
-{
-  id: 6,
-  name: "basenji",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10822.jpg"
-},
-{
-  id: 7,
-  name: "beagle",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10832.jpg"
-},
-{
-  id: 8,
-  name: "bluetick",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10982.jpg"
-},
-{
-  id: 9,
-  name: "borzoi",
-  image:   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11006.jpg"
-},
-{
-  id: 10,
-  name: "bouvier",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_11172.jpg"
-},
-{
-  id: 11,
-  name: "boxer",
-  image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_11182.jpg"
-}]
+// var data = [{
+//   id: 1,
+//   name: "affenpinscher",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"
+// },
+// {
+//   id: 2,
+//   name: "african",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg"
+// },
+// {
+//   id: 3,
+//   name: "airedale",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg"
+// },
+// {
+//   id: 4,
+//   name: "akita",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"
+// },
+// {
+//   id: 5,
+//   name: "appenzeller",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg"
+// },
+// {
+//   id: 6,
+//   name: "basenji",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10822.jpg"
+// },
+// {
+//   id: 7,
+//   name: "beagle",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10832.jpg"
+// },
+// {
+//   id: 8,
+//   name: "bluetick",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_10982.jpg"
+// },
+// {
+//   id: 9,
+//   name: "borzoi",
+//   image:   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11006.jpg"
+// },
+// {
+//   id: 10,
+//   name: "bouvier",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_11172.jpg"
+// },
+// {
+//   id: 11,
+//   name: "boxer",
+//   image: "https://images.dog.ceo/breeds/hound-afghan/n02088094_11182.jpg"
+// }]
 
-// let breeds = {
-//   "affenpinscher": [],
-//   "african": [],
-//   "airedale": [],
-//   "akita": [],
-//   "appenzeller": [],
-//   "basenji": [],
-//   "beagle": [],
-//   "bluetick": [],
-//   "borzoi": [],
-//   "bouvier": [],
-//   "boxer": []
-// }
+let breeds = {
+  "affenpinscher": [],
+  "african": [],
+  "airedale": [],
+  "akita": [],
+  "appenzeller": [],
+  "basenji": [],
+  "beagle": [],
+  "bluetick": [],
+  "borzoi": [],
+  "bouvier": [],
+  "boxer": []
+}
 
-// let dogImages = [
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10822.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10832.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10982.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11006.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11172.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11182.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1126.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1128.jpg",
-//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11432.jpg"
-//   ]
+let dogImages = [
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_10822.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_10832.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_10982.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_11006.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_11172.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_11182.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_1126.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_1128.jpg",
+  "https://images.dog.ceo/breeds/hound-afghan/n02088094_11432.jpg"
+  ]
 
   export default data;
