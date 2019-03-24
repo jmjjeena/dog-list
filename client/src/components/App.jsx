271a2a846a54f1e3062d00eeb6e0b348b038bbff
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-03-24 16:42:22 -0700

    Small changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,34 +4,55 @@ import DogSearch from './DogSearch.jsx';
 import DogImage from './DogImage.jsx';
 
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dogs: []
+    }
     // Binding event handler methods to an instance
     this.search = this.search.bind(this);
-    this.state = {
-      dog: []
-    }
+    // this.getList=this.getList.bind(this);
+    this.setState=this.setState.bind(this);
   }
 
-  // componentDidMount()
+  // componentDidMount() {
+  //   this.getList();
+  // }
+
+  // getList() {
+  //   console.log("request data");
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: "/",
+  //     success: function(breed) {
+  //       this.setState({
+  //         dogs: JSON.parse(breed)
+  //       })
+  //     },
+  //     error: function(error) {
+  //       console.log('Error' ,error)
+  //     }
+  //   })
+  // }
 
   search(breed) {
     console.log(`${breed} was searched`);
     $.ajax({
       method: "POST",
-      url: '/dogs',
+      url: '/',
       contentType: "application/json",
       dataType: "json",
       data: JSON.stringify({dogBeed: breed}),
-      success: () => {
-        console.log('The breed is: ', breed);
-      }
+      success: console.log('The breed is: ', breed)
     })
   }
 
   render () {
+   var searched;
+   if(this.state.search !== null) {
+     searched = <div>{this.state.search}</div>
+   }
     return (
       <div>
         <div>
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 res.send('Hello');
 });
 
-app.post('/dogs', function(req, res) {
+app.post('/', function(req, res) {
   res.send("Received");
 })
 
