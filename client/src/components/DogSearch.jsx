5357d40be71bdf5a0209233a167351942e4de23d
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-03-24 13:27:08 -0700

    Change the Dog search component to class component

--- client/src/components/DogSearch.jsx
+++ client/src/components/DogSearch.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
+class DogSearch extends React.Component{
+  constructor() {
+    super();
+    this.state = ({
+      value : ''
+    })
 
-var DogSearch = function() {
-  return (
-    <div className="dog-search-bar">
-      <input className="form-control" type="text" />
-      <button className="btn hidden-sm-down">Fetch</button>
-    </div>
-  )
+  }
+
+  render () {
+    return (
+      <div className="dog-search-bar">
+        <input className="form-control" type="text" />
+        <button className="btn hidden-sm-down">Fetch</button>
+      </div>
+    )
+  }
 }
 
 export default DogSearch;
