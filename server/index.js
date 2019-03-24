fd240c5bd55d9ef5a692449680c2efcdacac1802
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-03-24 11:13:44 -0700

    Implement GET request

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 // var db = require('../database-mysql');
 
 var app = express();
 
-// app.use( bodyParser.json() );
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use( bodyParser.json() );
+app.use(express.static(__dirname + '/../client/dist'));
 
 
 
-app.get('/api/users', function (req, res) {
-
+app.get('/', function (req, res) {
+res.send('Hello');
 });
 
 app.listen(3000, function() {
