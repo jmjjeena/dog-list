44555029c3301729860c0e1ea6e242ebf501cd96
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-03-24 10:34:09 -0700

    render hello world

--- react-client/src/mock-data.js
+++ react-client/src/mock-data.js
@@ -68,21 +68,21 @@ var data = [{
 //   "boxer": []
 // }
 
-let dogImages = [
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_10822.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_10832.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_10982.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_11006.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_11172.jpg",
-  // "https://images.dog.ceo/breeds/hound-afghan/n02088094_11182.jpg",
-  "https://images.dog.ceo/breeds/hound-afghan/n02088094_1126.jpg",
-  "https://images.dog.ceo/breeds/hound-afghan/n02088094_1128.jpg",
-  "https://images.dog.ceo/breeds/hound-afghan/n02088094_11432.jpg"
-  ]
+// let dogImages = [
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10822.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10832.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_10982.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11006.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11172.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11182.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1126.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_1128.jpg",
+//   "https://images.dog.ceo/breeds/hound-afghan/n02088094_11432.jpg"
+//   ]
 
   export default data;
